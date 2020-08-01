e1cb12a17ed7ebc1de59a1eb5c4bc3ce00ecebb7
verma26shubhi <verma.26shubhi@gmail.com>
2020-08-01 20:40:23 +0530

    Added Admin and AdminDecision

--- BankProject/CreateTables.sql
+++ BankProject/CreateTables.sql
@@ -13,3 +13,7 @@ create table DebitCard(DebitCardNumber Bigint primary key,UserID int references 
 
 create table Register(TempUserID int primary key, FirstName nvarchar(30) not null, SecondName nvarchar(30) not null, MiddleName nvarchar(30),Email nvarchar(20) unique, Phone Bigint not null,Gender varchar(10), AdhaarNumber Bigint not null Unique, FathersName nvarchar(30), DOB Date,
 PermanentAddress nvarchar(200) not null, CuurentAddress nvarchar(200) not null,OccupationType nvarchar(30), SourceofIncome nvarchar(20),AnnualSalary float, DebitCardOpted binary,NetBankingOpted binary,DateofApplication Date)
+
+create table Admin(AdminID int primary key, Password nvarchar(30))
+
+create table AdminDecision(AdminID int references Admin(AdminID), TempUserID int references Register(TempUserID), ApprovalGiven binary, DecisionDate Date)
