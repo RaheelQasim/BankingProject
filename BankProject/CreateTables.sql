baf565e987df9788e17a6dcb05aa956b49178373
RaheelQasim <68836429+RaheelQasim@users.noreply.github.com>
2020-08-01 17:49:32 +0500

    Update CreateTables.sql

--- BankProject/CreateTables.sql
+++ BankProject/CreateTables.sql
@@ -10,3 +10,6 @@ create table NetBanking(NetbankingID int primary key,UserID int references Users
 create table Transactions(TransactionID int primary key,UserID int references Users(Userid), Type nvarchar(20) not null, Date Date not null,Amount float(6) not null)
 
 create table DebitCard(DebitCardNumber Bigint primary key,UserID int references Users(Userid), CVV smallint not null,DateOfIssue Date not null, DateOfExpiry Date not null,PIN int not null)
+
+create table Register(TempUserID int primary key, FirstName nvarchar(30) not null, SecondName nvarchar(30) not null, MiddleName nvarchar(30),Email nvarchar(20) unique, Phone Bigint not null,Gender varchar(10), AdhaarNumber Bigint not null Unique, FathersName nvarchar(30), DOB Date,
+PermanentAddress nvarchar(200) not null, CuurentAddress nvarchar(200) not null,OccupationType nvarchar(30), SourceofIncome nvarchar(20),AnnualSalary float, DebitCardOpted binary,NetBankingOpted binary,DateofApplication Date)
